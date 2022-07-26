36827f88c331567fa90047d706c195d7678c899f
Vladimir <>
2022-07-26 20:16:55 +0300

    tabs

--- src/components/space.vue
+++ src/components/space.vue
@@ -1,23 +1,47 @@
 <template>
-	<div class="tables-view" :class="{
-		'is-loading': isLoading,
-		'is-empty': !isLoading && isTablesListEmpty
-	}">
+	<div
+		class="tables-view"
+		:class="{
+			'is-loading': isLoading,
+			'is-empty': !isLoading && isTablesListEmpty,
+		}"
+	>
 		<div class="grid-controls">
-			<label for="grid-size">Grid size: {{gridSize}}</label>
-			<input id="grid-size" type="range" v-model="gridSize" min="10" max="40">
-			<label for="grid-width">Grid width: {{gridWidth}}</label>
-			<input id="grid-width" type="range" v-model="gridWidth" min="10" max="40">
-			<label for="grid-height">Grid height: {{gridHeight}}</label>
-			<input id="grid-height" type="range" v-model="gridHeight" min="10" max="40">
+			<label for="grid-size">Grid size: {{ gridSize }}</label>
+			<input id="grid-size" type="range" v-model="gridSize" min="10" max="40" />
+			<label for="grid-width">Grid width: {{ gridWidth }}</label>
+			<input
+				id="grid-width"
+				type="range"
+				v-model="gridWidth"
+				min="10"
+				max="40"
+			/>
+			<label for="grid-height">Grid height: {{ gridHeight }}</label>
+			<input
+				id="grid-height"
+				type="range"
+				v-model="gridHeight"
+				min="10"
+				max="40"
+			/>
 		</div>
 		<div class="tables-block">
-			<div class="tables-grid" ref="grid" :style="{
-				width: getGridWidth,
-				height: getGridHeight,
-				backgroundSize: ''.concat(gridSize, 'px'),
-				backgroundPosition: ''.concat((gridSize / 2) * -1, 'px ', (gridSize / 2) * -1, 'px')
-			}">
+			<div
+				class="tables-grid"
+				ref="grid"
+				:style="{
+					width: getGridWidth,
+					height: getGridHeight,
+					backgroundSize: ''.concat(gridSize, 'px'),
+					backgroundPosition: ''.concat(
+						(gridSize / 2) * -1,
+						'px ',
+						(gridSize / 2) * -1,
+						'px'
+					),
+				}"
+			>
 				<div
 					class="table-wrapper"
 					:style="{
@@ -66,15 +90,14 @@
 			<div class="tables-control">
 				<div class="tables-control__new-tables">
 					<div class="table table--placeholder" @click="onCreateNewTable"></div>
-					<div class="table circle table--placeholder" @click="onCreateNewTable(true)"></div>
+					<div
+						class="table circle table--placeholder"
+						@click="onCreateNewTable(true)"
+					></div>
 				</div>
 				<div class="tables-control__actions">
-					<button @click="onReset" :disabled="isBtnsDisabled">
-						Отмена
-					</button>
-					<button @click="onSave" :disabled="isBtnsDisabled">
-						Сохранить
-					</button>
+					<button @click="onReset" :disabled="isBtnsDisabled">Отмена</button>
+					<button @click="onSave" :disabled="isBtnsDisabled">Сохранить</button>
 				</div>
 			</div>
 		</div>
@@ -88,62 +111,66 @@ import { computed } from '@vue/reactivity';
 
 const props = defineProps({
 	tables: Array,
-	isLoading: Boolean
-})
-const emit = defineEmits(['on-save'])
+	isLoading: Boolean,
+});
+const emit = defineEmits(['on-save']);
 
 const gridSize = ref(20);
 const gridWidth = ref(37);
 const gridHeight = ref(23);
 const BG_GRID_SIZE = 20;
 
-
 const dragItemIndex = ref(-1);
 const resizeItemIndex = ref(-1);
 const resizeYItemIndex = ref(-1);
 const grid = ref();
-const gridBoundingPosition = ref()
+const gridBoundingPosition = ref();
 
 watch([gridSize, gridWidth, gridHeight], () => {
-	_tables.value.forEach(transformTableData)
-})
-watch(() => props.tables, () => {
-	console.log(props.tables)
-	updateInnerTablesValue()
-})
+	_tables.value.forEach(transformTableData);
+});
+watch(
+	() => props.tables,
+	() => {
+		console.log(props.tables);
+		updateInnerTablesValue();
+	}
+);
 
 const _tables = ref([]);
-const _tablesSnapshot = ref()
-const isTablesListEmpty = computed(() => !_tables.value?.length)
+const _tablesSnapshot = ref();
+const isTablesListEmpty = computed(() => !_tables.value?.length);
 
 onMounted(() => {
-	document.querySelector('._tables-grid')
-	onResize()
+	document.querySelector('._tables-grid');
+	onResize();
 	document.addEventListener('mousedown', handleChanges);
 	document.addEventListener('mousemove', handleChanges);
 	document.addEventListener('touchmove', handleChanges);
 	document.addEventListener('mouseup', onTableDragEnd);
 	document.addEventListener('touchend', onTableDragEnd);
-	window.addEventListener('resize', onResize)
-	updateInnerTablesValue()
+	window.addEventListener('resize', onResize);
+	updateInnerTablesValue();
 });
 
 const updateInnerTablesValue = () => {
-	_tables.value = JSON.parse(JSON.stringify(props.tables || []))
+	_tables.value = JSON.parse(JSON.stringify(props.tables || []));
 	_tables.value = _tables.value.map(transformTableData);
-	_tablesSnapshot.value = JSON.stringify(_tables.value)
-}
+	_tablesSnapshot.value = JSON.stringify(_tables.value);
+};
 
 const onSave = () => {
-	emit('on-save', _tables.value)
-}
+	emit('on-save', _tables.value);
+};
 
 const onReset = () => {
-	_tables.value = JSON.parse(_tablesSnapshot.value).map(transformTableData)
-	_tablesSnapshot.value = JSON.stringify(_tables.value)
-}
+	_tables.value = JSON.parse(_tablesSnapshot.value).map(transformTableData);
+	_tablesSnapshot.value = JSON.stringify(_tables.value);
+};
 
-const isBtnsDisabled = computed(() => _tablesSnapshot.value === JSON.stringify(_tables.value))
+const isBtnsDisabled = computed(
+	() => _tablesSnapshot.value === JSON.stringify(_tables.value)
+);
 
 onBeforeUnmount(() => {
 	document.removeEventListener('mousedown', handleChanges);
@@ -151,31 +178,37 @@ onBeforeUnmount(() => {
 	document.removeEventListener('touchmove', handleChanges);
 	document.removeEventListener('mouseup', onTableDragEnd);
 	document.removeEventListener('touchend', onTableDragEnd);
-	window.removeEventListener('resize', onResize)
+	window.removeEventListener('resize', onResize);
 });
 
 const getGridWidth = computed(() => {
-	return ''.concat((gridSize.value * gridWidth.value) + (gridSize.value / BG_GRID_SIZE), 'px')
-})
+	return ''.concat(
+		gridSize.value * gridWidth.value + gridSize.value / BG_GRID_SIZE,
+		'px'
+	);
+});
 
 const getGridHeight = computed(() => {
-	return ''.concat((gridSize.value * gridHeight.value) + (gridSize.value / BG_GRID_SIZE), 'px')
-})
+	return ''.concat(
+		gridSize.value * gridHeight.value + gridSize.value / BG_GRID_SIZE,
+		'px'
+	);
+});
 
 function onResize() {
-	gridBoundingPosition.value = grid.value.getBoundingClientRect()
+	gridBoundingPosition.value = grid.value.getBoundingClientRect();
 }
 
 function onEditTableTitle(i) {
-	const result = prompt('Введите новое название стола', _tables.value[i].title)
+	const result = prompt('Введите новое название стола', _tables.value[i].title);
 	if (result) {
-		_tables.value[i].title = result
+		_tables.value[i].title = result;
 	}
 }
 
 const onDelete = (i) => {
-	_tables.value.splice(i, 1)
-}
+	_tables.value.splice(i, 1);
+};
 
 function handleChanges(e) {
 	if (resizeItemIndex.value !== -1) {
@@ -215,35 +248,34 @@ const throttleOnDrag = throttle(onTableDrag, 10);
 const throttleOnResize = throttle(onTableResize, 10);
 
 function onTableDrag(e, table) {
-	const {
-		clientX,
-		clientY,
-		updateOnDrag
-	} = initializeData(e, table);
+	const { clientX, clientY, updateOnDrag } = initializeData(e, table);
 
 	if (clientX === 0 && clientY === 0) {
 		return;
 	}
 
-	updateOnDrag()
+	updateOnDrag();
 
 	table = transformTableData(table);
 }
 
 function onTableResize(e, table) {
-	const {clientX, clientY, updateOnResize, updateOnResizeY} = initializeData(e, table) 
+	const { clientX, clientY, updateOnResize, updateOnResizeY } = initializeData(
+		e,
+		table
+	);
 	if (resizeYItemIndex.value !== -1) {
 		if (clientY === 0) {
 			return;
 		}
-		updateOnResizeY()
+		updateOnResizeY();
 		table = transformTableData(table, true);
 	}
 	if (resizeItemIndex.value !== -1) {
 		if (clientX === 0 && clientY === 0) {
 			return;
 		}
-		updateOnResize()
+		updateOnResize();
 		table = transformTableData(table, true);
 	}
 }
@@ -262,9 +294,9 @@ function onTableResizeStart(e, i) {
 	_tables.value[i].dragStartCoordinates = getEventCoordinates(e);
 }
 const getEventCoordinates = (e) => {
-	const {clientX, clientY} = e instanceof TouchEvent ? e.touches[0] : e
-	return [clientX, clientY]
-}
+	const { clientX, clientY } = e instanceof TouchEvent ? e.touches[0] : e;
+	return [clientX, clientY];
+};
 const transformTableData = (v, isResized = false) => {
 	if (isResized) {
 		v.width = Number(v.width.toFixed(9));
@@ -301,9 +333,9 @@ const transformTableData = (v, isResized = false) => {
 	v.left = getAsidePxUnits(v.x);
 	v.top = getAsidePxUnits(v.y);
 	return v;
-}
+};
 const initializeData = (e, table) => {
-	const [ clientX, clientY ] = getEventCoordinates(e);
+	const [clientX, clientY] = getEventCoordinates(e);
 
 	let xDiff = clientX - table.dragStartCoordinates[0];
 	let yDiff = clientY - table.dragStartCoordinates[1];
@@ -314,130 +346,145 @@ const initializeData = (e, table) => {
 	const isDecrementedByY = yDiff * -1 > gridSize.value;
 
 	const incrementXPosition = () => {
-		table.x = table.x + (xDiff / gridSize.value);
-		if ((table.x + table.width) > gridWidth.value) {
-			table.x = gridWidth.value - table.width
-		} 
+		table.x = table.x + xDiff / gridSize.value;
+		if (table.x + table.width > gridWidth.value) {
+			table.x = gridWidth.value - table.width;
+		}
 		table.dragStartCoordinates[0] = clientX;
 	};
 	const incrementYPosition = () => {
-		table.y = table.y + (yDiff / gridSize.value);
+		table.y = table.y + yDiff / gridSize.value;
 		table.dragStartCoordinates[1] = clientY;
-	}
+	};
 	const decrementXPosition = () => {
 		table.x = table.x - (xDiff * -1) / gridSize.value;
 		table.dragStartCoordinates[0] = clientX;
-	}
+	};
 	const decrementYPosition = () => {
 		table.y = table.y - (yDiff * -1) / gridSize.value;
 		table.dragStartCoordinates[1] = clientY;
-	}
+	};
 	const incrementWidth = () => {
-		table.width = table.width + (xDiff / gridSize.value);
+		table.width = table.width + xDiff / gridSize.value;
 		table.dragStartCoordinates[0] = clientX;
-	}
+	};
 	const incrementHeight = () => {
-		table.height = table.height + (yDiff / gridSize.value);
+		table.height = table.height + yDiff / gridSize.value;
 		table.dragStartCoordinates[1] = clientY;
-	}
+	};
 	const decrementWidth = () => {
-		table.width = table.width - ((xDiff * -1) / gridSize.value);
+		table.width = table.width - (xDiff * -1) / gridSize.value;
 		table.dragStartCoordinates[0] = clientX;
-	}
+	};
 	const decrementHeight = () => {
-		table.height = table.height - ((yDiff * -1) / gridSize.value);
+		table.height = table.height - (yDiff * -1) / gridSize.value;
 		table.dragStartCoordinates[1] = clientY;
-	}
+	};
 	const updateOnDrag = () => {
-		const {left, top, right, bottom} = gridBoundingPosition.value
+		const { left, top, right, bottom } = gridBoundingPosition.value;
 		if (left > clientX) {
-			xDiff = 0
-			table.x = 0
+			xDiff = 0;
+			table.x = 0;
 		}
 		if (top > clientY) {
-			yDiff = 0
-			table.y = 0
+			yDiff = 0;
+			table.y = 0;
 		}
 		if (right < clientX) {
-			xDiff = 0
-			table.x = gridWidth.value
+			xDiff = 0;
+			table.x = gridWidth.value;
 		}
 		if (bottom < clientY) {
-			yDiff = 0
-			table.y = gridHeight.value
+			yDiff = 0;
+			table.y = gridHeight.value;
 		}
 		if (isIncrementedByX) {
-			incrementXPosition()
+			incrementXPosition();
 		}
 		if (isIncrementedByY) {
-			incrementYPosition()
+			incrementYPosition();
 		}
 		if (isDecrementedByX) {
-			decrementXPosition()
+			decrementXPosition();
 		}
 		if (isDecrementedByY) {
-			decrementYPosition()
+			decrementYPosition();
 		}
-	}
+	};
 	const updateOnResize = () => {
-		updateOnResizeX()
-		updateOnResizeY()
-	}
+		updateOnResizeX();
+		updateOnResizeY();
+	};
 	const updateOnResizeX = () => {
-		const leftPosition = gridBoundingPosition.value.left + getTableNode().offsetLeft
+		const leftPosition =
+			gridBoundingPosition.value.left + getTableNode().offsetLeft;
 		if (leftPosition > clientX) {
-			xDiff = 0
-			table.width = 1
+			xDiff = 0;
+			table.width = 1;
 		}
 		if (isIncrementedByX) {
-			incrementWidth()
+			incrementWidth();
 		}
 		if (isDecrementedByX) {
-			decrementWidth()
+			decrementWidth();
 		}
-	}
+	};
 	const updateOnResizeY = () => {
-		const topPosition = gridBoundingPosition.value.top + getTableNode().offsetTop
+		const topPosition =
+			gridBoundingPosition.value.top + getTableNode().offsetTop;
 		if (topPosition > clientY) {
-			yDiff = 0
-			table.height = 1
+			yDiff = 0;
+			table.height = 1;
 		}
 		if (isIncrementedByY) {
-			incrementHeight()
+			incrementHeight();
 		}
 		if (isDecrementedByY) {
-			decrementHeight()
+			decrementHeight();
 		}
-	}
+	};
 	const getTableNode = () => {
 		if (dragItemIndex.value !== -1) {
-			return grid.value.children[dragItemIndex.value]
+			return grid.value.children[dragItemIndex.value];
 		}
 		if (resizeItemIndex.value !== -1) {
-			return grid.value.children[resizeItemIndex.value]
+			return grid.value.children[resizeItemIndex.value];
 		}
 		if (resizeYItemIndex.value !== -1) {
-			return grid.value.children[resizeYItemIndex.value]
+			return grid.value.children[resizeYItemIndex.value];
 		}
-	}
+	};
 
-	return { updateOnDrag, updateOnResize, updateOnResizeY, clientX, clientY, getTableNode };
-}
+	return {
+		updateOnDrag,
+		updateOnResize,
+		updateOnResizeY,
+		clientX,
+		clientY,
+		getTableNode,
+	};
+};
 const changeTableType = (table) => {
-	table.isRounded = !table.isRounded
-}
+	table.isRounded = !table.isRounded;
+};
 const onCreateNewTable = (rounded = false) => {
-	_tables.value.push(transformTableData({
-		width: 3,
-		height: 3,
-		title: ''.concat('Стол ', _tables.value.length + 1),
-		x: 0,
-		y: gridHeight.value - 3,
-		isRounded: rounded,
-	}))
-}
+	_tables.value.push(
+		transformTableData({
+			width: 3,
+			height: 3,
+			title: ''.concat('Стол ', _tables.value.length + 1),
+			x: 0,
+			y: gridHeight.value - 3,
+			isRounded: rounded,
+		})
+	);
+};
 const getAsidePxUnits = (v) =>
-	''.concat((Math.round((gridSize.value * v) / gridSize.value) * gridSize.value) + gridSize.value / BG_GRID_SIZE, 'px');
+	''.concat(
+		Math.round((gridSize.value * v) / gridSize.value) * gridSize.value +
+			gridSize.value / BG_GRID_SIZE,
+		'px'
+	);
 </script>
 
 <style lang="scss">
@@ -462,7 +509,7 @@ body.is-resizing-y {
 	cursor: s-resize;
 }
 .tables-view.is-loading {
-	opacity: .7;
+	opacity: 0.7;
 	pointer-events: none;
 	position: relative;
 	&:after {
@@ -532,8 +579,8 @@ body.is-resizing-y {
 		width: 60px;
 		height: 60px;
 		position: relative;
-		opacity: .6;
-		transition: .2s ease-in-out;
+		opacity: 0.6;
+		transition: 0.2s ease-in-out;
 		cursor: cell;
 		&:hover {
 			opacity: 1;
@@ -546,7 +593,7 @@ body.is-resizing-y {
 	left: 0;
 	height: 2px;
 	width: 100%;
-	background-color: rgba(rgb(80, 80, 80), .6);
+	background-color: rgba(rgb(80, 80, 80), 0.6);
 	cursor: s-resize;
 }
 .delete {
@@ -599,7 +646,7 @@ body.is-resizing-y {
 		text-align: center;
 		cursor: pointer;
 		&:hover {
-			opacity: .4;
+			opacity: 0.4;
 			background-color: white;
 		}
 	}
